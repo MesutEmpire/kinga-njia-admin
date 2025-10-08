cc4dfc0bd845a8d7fecd99fdc6fc2a66c7b71320
Samuel Wainaina <samwainaina01@gmail.com>
2025-10-08 08:58:27 +0300

    Created controller

---
+++ backend/src/main/java/com/kinganjia/backend/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.kinganjia.backend.controller;
+
+import com.kinganjia.backend.model.User;
+import com.kinganjia.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Iterable<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        System.out.println("USER :" + user.toString());
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @DeleteMapping
+    public void deleteAllUser() {
+        userService.deleteAllUsers();
+    }
+}
