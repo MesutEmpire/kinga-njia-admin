24d6c0713a41ea9efbe723b8be2c888742eb6801
Samuel Wainaina <samwainaina01@gmail.com>
2025-11-02 07:05:47 +0300

    Updated the image service

--- backend/src/main/java/com/kinganjia/backend/service/ImageService.java
+++ backend/src/main/java/com/kinganjia/backend/service/ImageService.java
@@ -1,41 +1,76 @@
 package com.kinganjia.backend.service;
 
+import com.kinganjia.backend.dto.ClaimResponseDTO;
+import com.kinganjia.backend.dto.ImageRequestDTO;
+import com.kinganjia.backend.dto.ImageResponseDTO;
+import com.kinganjia.backend.mapper.ImageMapper;
+import com.kinganjia.backend.model.Claim;
 import com.kinganjia.backend.model.Image;
 import com.kinganjia.backend.repository.ImageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class ImageService {
     private final ImageRepository imageRepository;
+    private final ImageMapper imageMapper;
 
-    public ImageService(ImageRepository imageRepository) {
+    public ImageService(ImageRepository imageRepository, ImageMapper imageMapper) {
         this.imageRepository = imageRepository;
+        this.imageMapper = imageMapper;
     }
 
-    public Optional<Image> getImageById(Long id) {
-        return imageRepository.findById(id);
+    public List<ImageResponseDTO> getAllImages() {
+        List<Image> images = imageRepository.findAll();
+        return images.stream().map(u -> imageMapper.toResponse(u)).collect(Collectors.toList());
     }
 
-    public Image createImage(Image image) {
-        return imageRepository.save(image);
+    public ImageResponseDTO getImageById(Long id) {
+        Image image = imageRepository.findById(id).orElseThrow(() -> new RuntimeException("Image not found"));
+        return imageMapper.toResponse(image);
     }
 
-    public Image updateImage(Image image) {
-        return imageRepository.save(image);
+    public List<ImageResponseDTO> getClaimByClaimId(Long id) {
+        List<Image> claims =imageRepository.findByClaimId(id).orElseThrow(() -> new RuntimeException("Image not found"));
+        return claims.stream().map(u -> imageMapper.toResponse(u)).collect(Collectors.toList());
     }
 
-    public List<Image> getAllImages() {
-        return imageRepository.findAll();
+    public ImageResponseDTO createImage(ImageRequestDTO imageDTO) {
+        Image image = imageMapper.toEntity(imageDTO);
+        Image createdImage = imageRepository.save(image);
+        return imageMapper.toResponse(createdImage);
     }
 
-    public void deleteImageById(Long id) {
+    public ImageResponseDTO updateImage(Long id, ImageRequestDTO imageDTO) {
+        Image existingImage = findImageById(id);
+        imageMapper.fullUpdateImageFromDto(imageDTO, existingImage);
+        log.info("Image update : {}",existingImage);
+        Image updatedImage = imageRepository.save(existingImage);
+        return imageMapper.toResponse(updatedImage);
+    }
+
+    public ImageResponseDTO partialUpdateImage(Long id, ImageRequestDTO imageUpdate) {
+        Image existingImage = findImageById(id);
+        imageMapper.updateImageFromDto(imageUpdate,existingImage);
+        log.info("Image partial update : {}",existingImage);
+        Image updatedImage = imageRepository.save(existingImage);
+        return imageMapper.toResponse(updatedImage);
+    }
+
+    public void deleteImage(Long id) {
         imageRepository.deleteById(id);
     }
 
     public void deleteAllImages() {
         imageRepository.deleteAll();
+    }
+
+    private Image findImageById(Long id){
+        return imageRepository.findById(id).orElseThrow(() -> new RuntimeException("Image not found with id: " + id));
     }
 }
