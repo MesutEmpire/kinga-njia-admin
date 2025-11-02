c8611d556bd44984aa87523e6c487bcec25ece61
Samuel Wainaina <samwainaina01@gmail.com>
2025-11-02 07:05:26 +0300

    Updated the image mapper

--- backend/src/main/java/com/kinganjia/backend/mapper/ImageMapper.java
+++ backend/src/main/java/com/kinganjia/backend/mapper/ImageMapper.java
@@ -1,14 +1,24 @@
 package com.kinganjia.backend.mapper;
 
+import com.kinganjia.backend.dto.ImageRequestDTO;
 import com.kinganjia.backend.dto.ImageResponseDTO;
 import com.kinganjia.backend.model.Image;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ImageMapper {
-    ImageMapper INSTANCE = Mappers.getMapper(ImageMapper.class);
-
     ImageResponseDTO toResponse(Image image);
-    Image toEntity(Image image);
+    Image toEntity(ImageRequestDTO dto);
+
+    // For partial updates - ignore null values
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateImageFromDto(ImageRequestDTO dto, @MappingTarget Image image);
+
+    // For full updates - update all values including nulls
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    void fullUpdateImageFromDto(ImageRequestDTO dto, @MappingTarget Image image);
 }
